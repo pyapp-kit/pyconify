219bd6bcc31d47f1647d5bbac5591d819c25a0cb
Grzegorz Bokota <bokota+github@gmail.com>
2023-10-09 03:44:57 +0000

    add test

--- tests/test_pyconify.py
+++ tests/test_pyconify.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pyconify
+import pyconify._cache
 import pytest
 
 
@@ -49,6 +50,10 @@ def test_tmp_svg(tmp_path) -> None:
     assert result2.parent == tmp_path
     assert result2 != result1
     assert result2.read_bytes() == pyconify.svg("bi", "alarm", rotate=90, box=True)
+
+    result3 = pyconify.svg_path("bi", "alarm-fill", rotate=90, box=True)
+    # assert str(result3).startswith(str(pyconify._cache.get_cache_directory()))
+
 
 
 def test_css() -> None:
